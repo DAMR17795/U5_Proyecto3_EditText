79bed53e4e9c7c0c943b7d46be2bb987f421664c
DANIEL2DAM <dmarrom577@g.educaand.es>
2022-11-08 12:31:43 +0100

    Initial commit

---
+++ app/src/main/java/www/iesmurgi/u5_proyecto3_edittext/MainActivity.kt
@@ -0,0 +1,11 @@
+package www.iesmurgi.u5_proyecto3_edittext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
